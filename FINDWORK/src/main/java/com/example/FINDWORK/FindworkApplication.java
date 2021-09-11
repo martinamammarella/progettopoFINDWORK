991bc101f7fae46f5206ae7ffc9a02cb6508a3f7
marty <marty@LAPTOP-G5ITQDIQ.homenet.telecomitalia.it>
2021-09-11 11:22:07 +0200

    commit1

---
+++ FINDWORK/src/main/java/com/example/FINDWORK/FindworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.FINDWORK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class FindworkApplication {
+	public static void main(String[] args) {
+			SpringApplication.run(FindworkApplication.class, args);
+		}
+
+	}
