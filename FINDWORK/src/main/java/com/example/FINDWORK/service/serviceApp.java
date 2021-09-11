4b307a6e20819e63dd721e048a8da34905e4d26f
marty <marty@LAPTOP-G5ITQDIQ.homenet.telecomitalia.it>
2021-09-11 11:47:39 +0200

    commitserviceappimplements

---
+++ FINDWORK/src/main/java/com/example/FINDWORK/service/serviceApp.java
@@ -0,0 +1,15 @@
+package com.example.FINDWORK.service;
+import com.example.FINDWORK.exception.bodyException;
+import com.example.FINDWORK.exception.parException;
+import com.example.FINDWORK.model.Lavori;
+import java.util.Date;
+import java.util.ArrayList;
+import org.json.simple.*;
+
+public interface serviceApp {
+	public ArrayList<Lavori> getOfferte();
+	public ArrayList<Lavori> analisiFiltro(ArrayList<Lavori>offerte,JSONArray city,JSONArray flin,String parametro) throws bodyException;
+	public ArrayList<Lavori>analisiStatsParametro(ArrayList<Lavori>offerte,String parametro)throws parException;
+	public ArrayList<Lavori> analisiFiltersStats(ArrayList<Lavori> offerte,JSONArray city,JSONArray linguaggi,String parametro,String link,String data) throws bodyException;
+
+}
