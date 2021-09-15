4b225e22229eece294959bc064fe3d858a795b86
marty <marty@LAPTOP-G5ITQDIQ.homenet.telecomitalia.it>
2021-09-15 18:23:24 +0200

    commit nuove eccezioni

---
+++ FINDWORK/src/main/java/com/example/FINDWORK/exception/cityException.java
@@ -0,0 +1,8 @@
+package com.example.FINDWORK.exception;
+
+public class cityException  extends Exception{
+	public cityException( String messaggio) {
+		super(messaggio);
+	}
+
+}
---
+++ FINDWORK/src/main/java/com/example/FINDWORK/exception/linguaggiException.java
@@ -0,0 +1,7 @@
+package com.example.FINDWORK.exception;
+
+public class linguaggiException extends Exception {
+public linguaggiException(String messaggio) {
+	super(messaggio);
+}
+}
